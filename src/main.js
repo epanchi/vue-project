daf9cc7f014ee651d602c74f72f830ba13f4a4d3
Edison Panchi <edison.panchi@gmail.com>
2025-02-17 06:49:13 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App,{
+    data(){
+        return {
+            counter: 0
+        }
+    }
+
+}).mount('#app')
